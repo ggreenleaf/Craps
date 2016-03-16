f4aa650bb9bbb9388ce893553d60d668fc84f979
Geoffrey Greenleaf <Geoffrey Greenleaf>
2016-03-16 09:37:27 -0500

    Craps project working on Dice rolling

---
+++ CrapsProject/Dice.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrapsProject
+{
+    class Dice
+    {
+        public void RollDice(Action<double> BiasFunc)
+        {
+            var res = BiasFunc(RandomUtil.RandomProbability());
+
+            
+        }
+
+        private int MapToResult(double x)
+        {
+
+            if (x <= 1)
+            {
+                return 2;
+            }
+            else if (x <= 3)
+            {
+                return 3;
+            }
+            else if (x <= 6)
+            {
+                return 4;
+            }
+            else if (x <= 10)
+            {
+                return 5;
+            }
+            else if (x <= 15)
+            {
+                return 6;
+            }
+            else if (x <= 21)
+            {
+                return 7;
+            }
+            else if (x <= 26)
+            {
+                return 8;
+            }
+            else if (x <= 30)
+            {
+                return 9;
+            }
+            else if (x <= 33)
+            {
+                return 10;
+            }
+            else if (x <= 35)
+            {
+                return 11;
+            }
+            else
+            {
+                return 12;
+            }
+        }
+    }
+}
---
+++ CrapsProject/RandomUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrapsProject
+{
+    class RandomUtil
+    {
+        private static Random rand = new Random();
+
+        public static double RandomProbability()
+        {
+           return rand.NextDouble();
+        } 
+    }
+}
